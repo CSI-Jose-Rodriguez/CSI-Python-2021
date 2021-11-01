6a76cd2f7894d4faa53025e3c52a3725ba7ea3b5
CSI-Jose-Rodriguez <89258522+CSI-Jose-Rodriguez@users.noreply.github.com>
2021-11-01 08:55:24 -0400

    Updated variables

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,15 +1,21 @@
-gun = "DVL-10"
-caliber = "7.62x51mm NATO"
-ammunition = "7.62x51mm M80"
-velocity_ms = 833
-
-building = "Soleil"
-buildingHeight = 288
-
-gravity_ms = 9.81
-
 import math
 
-time_s = math.sqrt((2 * buildingHeight) / gravity_ms)
+# gun = "DVL-10"
+# caliber = "7.62x51mm NATO"
+# ammunition = "7.62x51mm M80"
+# velocity_ms = 833
 
-print(f"The gun slected for the experiment is {gun}. Its claiber is {caliber} and uses {ammunition} as its ammunition. It shoots at a velocity of {velocity_ms}m/s. It is being shot from {building} that is {buildingHeight} feet above the ground.")
+# building = "Soleil"
+# buildingHeight = 288
+
+# gravity_ms = 9.81
+
+def ProjectileFunction(gun:str, caliber:str, ammunition:str, velocity_ms:int, building:str, buildingHeight:int, gravity_ms:int):
+    time_s = math.sqrt((2 * buildingHeight) / gravity_ms)
+    
+    distance_m = (velocity_ms * time_s)
+
+    print(f"The gun slected for the experiment is {gun}. Its claiber is {caliber} and uses {ammunition} as its ammunition. It shoots at a velocity of {velocity_ms}m/s. It is being shot from {building} that is {buildingHeight} feet above the ground.")
+
+
+ProjectileFunction("DVL-10", "7.62x51mm NATO", "7.62x51mm M80", 833, "Soleil", 288, 9.81)
