b03300ca1905d4f6cc003994e6cc935a7bbb43d6
CSI-Jose-Rodriguez <89258522+CSI-Jose-Rodriguez@users.noreply.github.com>
2021-11-05 09:13:23 -0400

    Made the ExperimentalData class

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,4 +1,5 @@
 import math
+from ExperimentalData import ExperimentalData
 
 # gun = "DVL-10"
 # caliber = "7.62x51mm NATO"
@@ -10,12 +11,26 @@ import math
 
 # gravity_ms = 9.81
 
-def ProjectileFunction(gun:str, caliber:str, ammunition:str, velocity_ms:int, building:str, buildingHeight:int, gravity_ms:int):
-    time_s = math.sqrt((2 * buildingHeight) / gravity_ms)
+def ProjectileFunction(experimentalData: ExperimentalData):
+    time_s = math.sqrt((2 * experimentalData.buildingHeight) / experimentalData.gravity_ms)
     
-    distance_m = (velocity_ms * time_s)
+    distance_m = (experimentalData.velocity_ms * time_s)
+    # distance_m = (experimentalData[velocity_ms] * time_s)
 
-    print(f"The gun slected for the experiment is {gun}. Its claiber is {caliber} and uses {ammunition} as its ammunition. It shoots at a velocity of {velocity_ms}m/s. It is being shot from {building} that is {buildingHeight} feet above the ground.")
+    print(f"The gun slected for the experiment is {experimentalData.gun}. Its claiber is {experimentalData.caliber} and uses {experimentalData.ammunition} as its ammunition. It shoots at a velocity of {experimentalData.velocity_ms}m/s. It is being shot from {experimentalData.building} that is {experimentalData.buildingHeight} feet above the ground.")
 
+# experimentalData = {
 
-ProjectileFunction("DVL-10", "7.62x51mm NATO", "7.62x51mm M80", 833, "Soleil", 288, 9.81)
+# "gun": "DVL-10",
+# "caliber": "7.62x51mm NATO",
+# "ammunition": "7.62x51mm M80",
+# "velocity_ms": 833,
+# "building": "Soleil",
+# "buildingHeight": 288,
+# "gravity_ms": 9.81
+
+# }
+
+experimentalData = ExperimentalData("DVL-10", "7.62x51mm NATO", "7.62x51mm M80", 833, "Soleil", 288, 9.81)
+
+ProjectileFunction
