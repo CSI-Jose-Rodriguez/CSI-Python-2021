5138e6fa69628712ccdafe3668db39b368a7b603
CSI-Jose-Rodriguez <89258522+CSI-Jose-Rodriguez@users.noreply.github.com>
2022-01-26 09:23:20 -0400

    demasiado de hangman

--- Projects/Jose-Rodriguez-Hangman.py
+++ Projects/Jose-Rodriguez-Hangman.py
@@ -25,6 +25,38 @@ def play(word):
                 print("Isn't in the word :(")
                 tries -=1
                 guessed_letters.append(guess)
+            else:
+                print("Nice one", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate (word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already tried", guess, "!")
+            elif guess != word:
+                print(guess, "isn't the word")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+                print("invalid input")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Good job, you guessed the word!!!")
+    else:
+        print("I'm sorry, but you ran out of tries. The word was" + word + ". Try again.")
+            
+
+
 
 def display_hangman(tries):
  stages = [ """
