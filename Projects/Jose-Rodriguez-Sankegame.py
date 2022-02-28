5d95e808cac9fb5deb6ced469d3a70a6e4b2a457
CSI-Jose-Rodriguez <89258522+CSI-Jose-Rodriguez@users.noreply.github.com>
2022-02-28 09:20:34 -0400

    snak

--- Projects/Jose-Rodriguez-Sankegame.py
+++ Projects/Jose-Rodriguez-Sankegame.py
@@ -18,11 +18,17 @@ pygame.display.set_caption("Snake game by JD") # ARaneserá el nombre del jusse
 game_over=False
 
 snake_block = 10
-snake_speed = 25
+snake_speed = 15
 
 clock = pygame.time.Clock()
 
 font_style = pygame.font.SysFont(None, 25)
+score_font = pygame.font.SysFont(None, 25)
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, blue, [x[0], x[1], snake_block, snake_block])
+
 
 def message(msg,color):
     mesg = font_style.render(msg, True, color)
@@ -37,6 +43,9 @@ def gameRestart():
 
     x1_change = 0
     y1_change = 0
+
+    snake_List=[]
+    length_of_snake = 1
 
     foodx = round(random.randrange(0, dis_width - snake_block) /10) *10
     foody = round(random.randrange(0, dis_width - snake_block) /10) *10
@@ -80,8 +89,26 @@ def gameRestart():
         y1 += y1_change
         dis.fill(green)
         pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block]) #crea el food
-        pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block]) #crea el snake
+        #pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block]) #crea el snake
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[: -1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+
         pygame.display.update()   
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) /10) *10
+            foody = round(random.randrange(0, dis_width - snake_block) /10) *10
+            length_of_snake += 1
 
         clock.tick(snake_speed)
 
